6b791c5bbd1fa06085200c8daa7a89b3f3ebc8e0
Olivier Valette <olivier.valette@spi10.com>
2018-12-07 14:14:23 +0100

    API implement (Google custom search and swapi)

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -3,11 +3,19 @@ module.exports = {
     score: 0,
     lives: 5,
     deads: 0,
+    character: 26,
     audioDead: new Audio("https://www.soundjay.com/mechanical/sounds/gun-gunshot-01.mp3"),
     audioTrigger: new Audio("https://www.soundjay.com/mechanical/sounds/gun-trigger-click-01.mp3"),
+    // values for swapi API
+    characterSearchAPI: "https://swapi.co/api/people/",
+    // values for Google customsearch
+    // imageSearchAPI: "https://www.googleapis.com/customsearch/v1?key=AIzaSyBGEEg3ld_VC-T0qI-wx5xFVG5ElwOfo74&cx=002035746340990724895:hddqw2saqqg&q=",
+    // imageSearchParameters: "&searchType=image&fileType=jpg&imgType=face&alt=json",
     sUserName: prompt('Votre nom ?', 'Rick and Morty'),
     eUserName: document.getElementById('js-username'),
     eImage: document.getElementById('js-image'),
+    eCname: document.getElementById('js-cname'),
+    eCeyes: document.getElementById('js-ceyes'),
     eScore: document.getElementById('js-score'),
     eTrigger: document.getElementById('js-trigger'),
     eLives: document.getElementById('js-lives')
--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,35 @@ import helpers from './modules/helpers.js';
 // Fonction anonyme auto-appelante permettant de définir un alias
 // à l'objet settings (et helpers) de portée limitée
 ((s, h)=> {
+    
+    /* with xmlHttpRequest and 
+    // get images by Google customsearch API
+    const imageSearch = new XMLHttpRequest(JSON);
+    // asynchronous method: waiting for request response
+    imageSearch.onreadystatechange = function(event) {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                console.log("Réponse reçue: %s", this.responseText);
+            } else {
+                console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+            }
+        }
+    };
+    imageSearch.open('GET', s.imageSearchAPI + s.username + s.imageSearchParameters, false); 
+    imageSearch.send(null);
+    */
+    
+    // get images by swapi API
+    // with fetch()
+    // draw randomly a new character
+    s.character = Math.floor(Math.random()*50)+1;
+    fetch(s.characterSearchAPI + s.character + "/?format=json").then(function(response) {
+        response.json().then(function(json) {
+            s.eCname.innerText = json.name;
+            s.eCeyes.innerText = json.eye_color;
+        });
+    });
+    
     // update HTML elements
     eUpdate();
 
@@ -19,35 +48,39 @@ import helpers from './modules/helpers.js';
         // random draw...
         if (Math.random() > s.difficulty) {
             // live
-            ++s.score;
             s.audioTrigger.play();
+            ++s.score;
         }
         else {
             // shot
+            s.audioDead.play();
             --s.lives;
             ++s.deads;
-            s.audioDead.play();
             // end of game
-            if (s.deads == 5) {
+            if (s.lives == 0) {
                 
             }
         }
         // update HTML elements
         eUpdate();
     });
-
+    
     // Update HTML elements
     function eUpdate() {
         // username
         s.eUserName.innerText = s.sUserName;
         // score
-        s.eScore.innerText = 'Score: ' + s.score;
+        s.eScore.innerText = s.score;
         // livebar with skulls and hearts
         let liveBar = '';
         for (let i = 0; i < s.lives + s.deads; i++) {
             liveBar += ((i < s.deads) ? '💀' : '💛');
         }
         s.eLives.innerText = liveBar;
+        // TODO 
+        if (s.lives == 0) {
+                alert("End of game! You're definitly dead");
+        }
     }
 
 }) (settings, helpers);
